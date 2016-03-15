0e6741c401d5fb490aca303aac0b9480c6ac76fe
mooden <alexandrostruses@yandex.ru>
2016-03-15 08:22:58 +0400

    initial

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,20 @@
-.pipe( postcss([
-    require('postcss-nested'),
-    require('postcss-mixins'),
-    require('postcss-simple-vars')
+require('es6-promise').polyfill();
+
+var gulp = require('gulp'),
+	postcss = require('gulp-postcss'),
+	processors = [
+		require('postcss-import'),
+		require('postcss-mixins'),
+		require('postcss-simple-vars'),
+		require('postcss-nested'),
+		//require('autoprefixer-core')({ browsers: ['last 2 versions', '> 2%'] })
+	];
+
+
+gulp.task('css', function(){
+	return gulp.src('src/css/h5next.css')
+	.pipe(
+		postcss(processors)
+	)
+	.pipe(gulp.dest('dist/styles/'))
+})

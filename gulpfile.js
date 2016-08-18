ce8dd964f793872cebd24a98d5085ce24f9eec5f
mooden <alexandrostruses@yandex.ru>
2016-08-18 08:23:36 +0400

    bem grid

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,31 @@
 require('es6-promise').polyfill();
 
 var gulp = require('gulp'),
-	postcss = require('gulp-postcss'),
-	cssmin = require('gulp-cssmin'),
-	rename = require('gulp-rename'),
-	processors = [
-		require('postcss-import'),
-		require('postcss-mixins'),
-		require('postcss-simple-vars'),
-		require('postcss-nested')
-	];
+    postcss = require('gulp-postcss'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    //csslint = require('gulp-csslint'),
+    processors = [
+        require('postcss-import'),
+        require('postcss-mixins'),
+        require('postcss-simple-vars'),
+        require('postcss-nested')
+    ];
 
 
 gulp.task('css', function(){
-	return gulp.src('src/css/h5next.css')
-	.pipe(
-		postcss(processors)
-	)
-	.pipe(gulp.dest('dist/styles/'))
+    return gulp.src('src/css/h5next.css')
+    .pipe(
+        postcss(processors)
+    )
+    //.pipe(csslint())
+    //.pipe(csslint.reporter())
+    .pipe(gulp.dest('dist/styles/'))
 })
 
 gulp.task('css-min', function() {
-	return gulp.src('dist/styles/h5next.css')
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('dist/styles/'))
+    return gulp.src('dist/styles/h5next.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/styles/'))
 })
